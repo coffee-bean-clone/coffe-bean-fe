f77db1c28f8bd110d9c99d3e235858134520aba2
summermong <2514o_o@naver.com>
2024-03-29 00:06:50 +0900

    :sparkles: FEAT: footer 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Components/Header';
+import styled from 'styled-components';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './Pages/Main';
 import Login from './Pages/Login';
@@ -8,24 +9,36 @@ import Store from './Pages/Store';
 import Recruit from './Pages/Recruit';
 import HeavyBuying from './Pages/HeavyBuying';
 import Service from './Pages/Service';
-import Footer from './Components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/recruit" element={<Recruit />} />
-        <Route path="/heavybuying" element={<HeavyBuying />} />
-        <Route path="/service" element={<Service />} />
-      </Routes>
-      <Footer />
+      <Wrapper>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/recruit" element={<Recruit />} />
+          <Route path="/heavybuying" element={<HeavyBuying />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+      </Wrapper>
+      <Footer>footer</Footer>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  min-height: 100vh; /* 최소 높이 설정 */
+  position: relative; /* Wrapper를 relative로 설정 */
+  padding-bottom: 50px; /* Footer 높이만큼 Wrapper의 하단 패딩 추가 */
+  z-index: 999;
+`;
+
+const Footer = styled.div`
+  background-color: lightgray;
+`;
