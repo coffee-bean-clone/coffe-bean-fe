0c388711745b6e2cb2fce6c413d2aebef1217476
summermong <2514o_o@naver.com>
2024-04-11 23:12:37 +0900

    :sparkles: FEAT: 메인 페이지의 product 캐러셀 수정

--- src/Components/Content.tsx
+++ src/Components/Content.tsx
@@ -23,65 +23,113 @@ interface Product {
 }
 
 const Content = () => {
-  const [mdPickArr, setMdPickArr] = useState<Product[]>([]);
+  const [newProductArr, setNewProductArr] = useState<Product[]>([]);
+  const [saleProductArr, setSaleProductArr] = useState<Product[]>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchNewProductData = async () => {
       try {
-        const { data } = await axiosInstance.get<Product[]>(`/product/sale`);
-        setMdPickArr(data);
+        const { data } = await axiosInstance.get<Product[]>(`/product/new`);
+        setNewProductArr(data);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
+    fetchNewProductData();
+  }, []);
 
-    fetchData();
+  useEffect(() => {
+    const fetchSaleProductData = async () => {
+      try {
+        const { data } = await axiosInstance.get<Product[]>(`/product/sale`);
+        setSaleProductArr(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchSaleProductData();
   }, []);
 
   return (
     <ContentContainer>
-      <MdPick>MD`s Pick</MdPick>
-      <StyledSwiperContainer>
-        <Swiper
-          breakpoints={{
-            768: {
-              slidesPerView: 2,
-            },
-            1024: {
-              slidesPerView: 4,
-            },
-          }}
-          autoplay={true}
-          speed={300}
-          modules={[Pagination, Autoplay]}
-          spaceBetween={20}
-        >
-          {mdPickArr.map(product => (
-            <SwiperSlide key={product._id}>
-              <SlideImg src={product.productImages[0]} />
-              <SlideTitle>{product.title}</SlideTitle>
-              <SlideTitle>{product.price}원</SlideTitle>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </StyledSwiperContainer>
+      <ProductContent>
+        <ContentTitle>New Product</ContentTitle>
+        <StyledSwiperContainer>
+          <Swiper
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 4,
+              },
+            }}
+            autoplay={true}
+            speed={300}
+            loop={true}
+            modules={[Pagination, Autoplay]}
+            spaceBetween={20}
+          >
+            {newProductArr.map(product => (
+              <SwiperSlide key={product._id}>
+                <SlideImg src={product.productImages[0]} />
+                <SlideTitle>{product.title}</SlideTitle>
+                <SlideTitle>{product.price}원</SlideTitle>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </StyledSwiperContainer>
+      </ProductContent>
+      <ProductContent>
+        <ContentTitle>Sale Product</ContentTitle>
+        <StyledSwiperContainer>
+          <Swiper
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 4,
+              },
+            }}
+            autoplay={true}
+            speed={300}
+            loop={true}
+            modules={[Pagination, Autoplay]}
+            spaceBetween={20}
+          >
+            {saleProductArr.map(product => (
+              <SwiperSlide key={product._id}>
+                <SlideImg src={product.productImages[0]} />
+                <SlideTitle>{product.title}</SlideTitle>
+                <SlideTitle>{product.price}원</SlideTitle>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </StyledSwiperContainer>
+      </ProductContent>
     </ContentContainer>
   );
 };
 
 const ContentContainer = styled.div`
-  padding: 30px 0px;
+  padding: 10px 0px;
 `;
 
-const MdPick = styled.h2`
+const ProductContent = styled.div`
+  padding: 30px;
+`;
+
+const ContentTitle = styled.h2`
   text-align: center;
   padding: 30px;
   color: orange;
 `;
 
 const StyledSwiperContainer = styled.div`
-  width: 80%;
+  text-align: center;
   margin: 0 auto;
+  width: 80%;
 `;
 
 const SlideTitle = styled.div`
@@ -89,7 +137,8 @@ const SlideTitle = styled.div`
 `;
 
 const SlideImg = styled.img`
-  width: 80%;
+  margin: 0 auto;
+  width: 70%;
 `;
 
 export default Content;
