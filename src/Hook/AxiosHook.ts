f010815e0d5094e530f7776c28ff95a38fd42ec5
summermong <2514o_o@naver.com>
2024-05-01 00:08:54 +0900

    :sparkles: FEAT: token에 따른 axios hook 수정

--- src/Hook/AxiosHook.ts
+++ src/Hook/AxiosHook.ts
@@ -2,4 +2,32 @@ import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  config => {
+    const accessToken = localStorage.getItem('Authorization');
+
+    config.headers['Content-Type'] = 'application/json';
+    config.headers['Authorization'] = `Bearer ${accessToken}`;
+
+    return config;
+  },
+
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  response => {
+    console.log(response);
+    return response;
+  },
+
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
