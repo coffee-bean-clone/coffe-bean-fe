276c373aecfa36f211f79628bef1cfeabf2e9ddb
summermong <2514o_o@naver.com>
2024-05-01 00:04:13 +0900

    :truck: RENAME: 폴더명 type -> util로 수정

---
+++ src/util/ProductType.tsx
@@ -0,0 +1,13 @@
+export type ProductType = {
+  _id: string;
+  title: string;
+  price: number;
+  mainCategory: string;
+  subCategory: string;
+  isSale: boolean;
+  isNew: boolean;
+  detailImage: string;
+  productImages: string[];
+  createdAt: string;
+  __v: number;
+};
